9426c6d81a037d0a171f64c0515ad24de28a7780
Mahdi Heidari <mahdih3idari@gmail.com>
2023-01-26 22:40:00 +0330

    Create dnsUtil.py

---
+++ modules/dnsUtil.py
@@ -0,0 +1,118 @@
+
+Do53_URLS = {
+    # Ipv4_Default
+    'ES - puntCAT': ['109.69.8.51'],
+    'US - Hurricane Electric': ['74.82.42.42'],
+    'US - Google Public DNS': ['8.8.8.8', '8.8.4.4'],
+    'US - OpenDNS': ['208.67.222.222', '208.67.220.220'],
+    'US - OpenDNS - 2': ['208.67.222.220', '208.67.220.222'],
+    'RU - Yandex': ['77.88.8.1', '77.88.8.8'],
+    'AU - Cloudflare': ['1.1.1.1', '1.0.0.1'],
+    'AU - Cloudflare security': ['1.1.1.2', '1.0.0.2'],
+    'AU - Cloudflare Family': ['1.1.1.3', '1.0.0.3'],
+    'US - Norton ConnectSafe Basic': ['199.85.126.10', '199.85.127.10'],
+    'US - Level 3 - A': ['209.244.0.3', '209.244.0.4'],
+    'US - Level 3 - B': ['4.2.2.1', '4.2.2.2'],
+    'US - Level 3 - C': ['4.2.2.3', '4.2.2.4'],
+    'US - Level 3 - D': ['4.2.2.5', '4.2.2.6'],
+    'US - Comodo Secure': ['8.26.56.26', '8.20.247.20'],
+    'US - Dyn': ['216.146.35.35', '216.146.36.36'],
+    'US - Norton DNS': ['198.153.192.1', '198.153.194.1'],
+    'US - Comodo': ['156.154.70.22', '156.154.71.22'],
+    'US - VeriSign Public DNS': ['64.6.64.6', '64.6.65.6'],
+    'US - Qwest': ['205.171.3.65', '205.171.2.65'],
+    'US - Sprint': ['204.97.212.10', '204.117.214.10'],
+    'DK - Censurfridns': ['89.233.43.71', '91.239.100.100'],
+    'RU - Safe DNS': ['195.46.39.39', '195.46.39.40'],
+    'DE - DNS WATCH': ['84.200.69.80', '84.200.70.40'],
+    'AT - FreeDNS': ['37.235.1.174', '37.235.1.177'],
+    'US - Sprintlink': ['199.2.252.10', '204.97.212.10'],
+    'US - UltraDNS': ['204.69.234.1', '204.74.101.1'],
+    'GB - Zen Internet': ['212.23.8.1', '212.23.3.1'],
+    'GB - Orange DNS': ['195.92.195.94', '195.92.195.95'],
+    'NL - Freenom World': ['80.80.80.80', '80.80.81.81'],
+    'FR - FDN': ['80.67.169.12', '80.67.169.40'],
+    'US - Neustar 1': ['156.154.70.1', '156.154.71.1'],
+    'US - Neustar 2': ['156.154.70.5', '156.154.71.5'],
+    'RU - AdGuard': ['176.103.130.130', '176.103.130.131'],
+    'US - Quad9 Security': ['9.9.9.9', '149.112.112.112'],
+    'US - Quad9 No Security': ['9.9.9.10', '149.112.112.10'],
+    'BG - MegaLan': ['95.111.55.251', '95.111.55.250'],
+    'IR - shecan': ['178.22.122.100', '185.51.200.2'],
+
+    # Ipv4_Family
+    'US - OpenDNS Family': ['208.67.222.123', '208.67.220.123'],
+    'US - Norton ConnectSafe Family': ['199.85.126.30', '199.85.127.30'],
+    'RU - Yandex Family': ['77.88.8.7', '77.88.8.3'],
+    'IE - CleanBrowsing Family': ['185.228.168.168', '185.228.169.168'],
+    'IE - CleanBrowsing Adult Filter': ['185.228.168.10', '185.228.169.11'],
+    'US - Neustar Family Secure': ['156.154.70.3', '156.154.71.3'],
+    'US - Neustar Business Secure': ['156.154.70.4', '156.154.71.4'],
+    'RU - AdGuard Family': ['176.103.130.132', '176.103.130.134'],
+
+    # Ipv4_Secure
+    'US - Norton ConnectSafe Secure': ['199.85.126.20', '199.85.127.20'],
+    'RU - Yandex Safe': ['77.88.8.88', '77.88.8.2'],
+    'US - Comodo Secure': ['8.26.56.26', '8.20.247.20'],
+    'IE - CleanBrowsing Secure': ['185.228.168.9', '185.228.169.9'],
+    'US - NeustarThreat Protection': ['156.154.70.2', '156.154.71.2'],
+}
+
+Do53_URLS_v6 = {
+    # Ipv6_Default
+    'US - Google Public DNS': ['2001:4860:4860::8888', '2001:4860:4860::8844'],
+    'US - OpenDNS Sandbox': ['2620:0:ccc::2', '2620:0:ccd::2'],
+    'RU - Yandex DNS': ['2a02:6b8::feed:0ff', '2a02:6b8:0:1::feed:0ff'],
+    'DK - Censurfridns': ['2001:67c:28a4::', '2002:d596:2a92:1:71:53::'],
+    'US - Cesidian Root': ['2001:470:6c:521::2', '2001:470:6d:521::1'],
+    'US - DNS WATCH': ['2001:1608:10:25::1c04:b12f', '2001:1608:10:25::9249:d69b'],
+    'US - FDN': ['2001:910:800::12', '2001:910:800::40'],
+    'AU - Cloudflare': ['2606:4700:4700::1111', '2606:4700:4700::1001'],
+    'US - Neustar 1': ['2610:a1:1018::1', '2610:a1:1019::1'],
+    'US - Neustar 2': ['2610:a1:1018::5', '2610:a1:1019::5'],
+    'RU - AdGuard': ['2a00:5a60::ad1:0ff', '2a00:5a60::ad2:0ff'],
+    'US - Quad9 Security': ['2620:fe::fe', '2620:fe::9'],
+    'US - Quad9 No Security': ['2620:fe::10', '2620:fe::fe:10'],
+
+    # Ipv6_Family
+    'RU - Yandex Family': ['2a02:6b8::feed:a11', '2a02:6b8:0:1::feed:a11'],
+    'IE - CleanBrowsing Family': ['2a0d:2a00:1::', '2a0d:2a00:2::'],
+    'IE - CleanBrowsing Adult Filter': ['2a0d:2a00:1::1', '2a0d:2a00:2::1'],
+    'US - Neustar Family Secure': ['2610:a1:1018::3', '2610:a1:1019::3'],
+    'US - Neustar Business Secure': ['2610:a1:1018::4', '2610:a1:1019::4'],
+    'RU - AdGuard Family': ['2a00:5a60::bad1:0ff', '2a00:5a60::bad2:0ff'],
+
+    # Ipv6_Secure
+    'RU - Yandex Safe': ['2a02:6b8::feed:bad', '2a02:6b8:0:1::feed:bad'],
+    'IE - CleanBrowsing Secure': ['2a0d:2a00:1::2', '2a0d:2a00:2::2'],
+    'US - NeustarThreat Protection': ['2610:a1:1018::2', '2610:a1:1019::2'], 
+}
+
+DoT_URLS = {
+    "google": ["tls://dns.google:853"],
+    "cloudflare": ["tls://one.one.one.one:853", "tls://security.cloudflare-dns.com:853", "tls://family.cloudflare-dns.com:853"],
+    "quad9": ["tls://dns.quad9.net:853"],
+    "cleanbrowsing": ["tls://doh.cleanbrowsing.org:853"],
+    "cira": ["tls://private.canadianshield.cira.ca:853"],
+    "cira-protect": ["tls://protected.canadianshield.cira.ca:853"],
+    "cira-family": ["tls://family.canadianshield.cira.ca:853"],
+    "libredns": ["tls://doh.libredns.gr:853"],
+    "libredns-ads": ["tls://doh.libredns.gr:853"],
+    "adguard": ["tls://dns.adguard.com:853"],
+    "adguard-family": ["tls://dns-family.adguard.com:853"],
+}
+
+DoH_URLS = {
+    "libredns": ["https://doh.libredns.gr/dns-query"],
+    "libredns-ads": ["https://doh.libredns.gr/ads"],
+    "google": ["https://dns.google/dns-query"],
+    "cloudflare": ["https://cloudflare-dns.com/dns-query", "https://security.cloudflare-dns.com/dns-query", "https://family.cloudflare-dns.com/dns-query"],
+    "quad9": ["https://dns.quad9.net/dns-query"],
+    "cleanbrowsing": ["https://doh.cleanbrowsing.org/doh/family-filter"],
+    "cleanbrowsing-secure": ["https://doh.cleanbrowsing.org/doh/security-filter"],
+    "cleanbrowsing-adult": ["https://doh.cleanbrowsing.org/doh/adult-filter"],
+    "cira": ["https://private.canadianshield.cira.ca/dns-query"],
+    "cira-protect": ["https://protected.canadianshield.cira.ca/dns-query"],
+    "cira-family": ["https://family.canadianshield.cira.ca/dns-query"],
+}
+
